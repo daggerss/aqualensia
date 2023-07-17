0ce0b003967ca137e9515966a18b8609139f0891
Dagny Eran Yenko <dagnyeran@gmail.com>
2023-07-17 09:21:22 +0800

    Apply movement

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private Rigidbody2D body;
+    private Vector2 moveInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
+
+        moveInput.Normalize();
+
+        body.velocity = moveInput * moveSpeed;
+    }
+}
